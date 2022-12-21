a81b3f84bb935b149ab8fff3a1a9a33da726cf7f
sameep25 <sameepvishwakarma@gmail.com>
2022-12-21 23:09:38 +0530

    made fetch chat api

--- server/controller/chatController.js
+++ server/controller/chatController.js
@@ -1,6 +1,7 @@
 import Chat from "../schema/chatSchema.js";
 import User from "../schema/userSchema.js";
 
+// access chat or create new chat
 export const accessChat = async (req, res) => {
   try {
     const { userId } = req.body;
@@ -18,30 +19,54 @@ export const accessChat = async (req, res) => {
     })
       .populate("users", "-password") //populating users array in Chat collection with user data expect password
       .populate("latestMessage");
-
+    // Populate will automatically replace the specified path in the document, with document(s) from other collection(s).
     isChat = await User.populate(isChat, {
       path: "latestMessage.sender",
       select: "name pic email",
     });
 
     // check if chat exists
-    if(isChat.length > 0){
-        res.send(isChat[0]) ;
-    }else{
+    if (isChat.length > 0) {
+      res.send(isChat[0]);
+    } else {
+      //creating new chat
+      const chatData = {
+        chatName: "sender",
+        isGroupChat: false,
+        users: [req.user._id, userId],
+      };
 
-        //creating new chat      
-        const chatData = {
-            chatName:"sender",
-            isGroupChat:false,
-            users:[req.user._id ,userId] ,
-        }
+      const createdChat = await Chat.create(chatData);
+      const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        "users",
+        "-password"
+      );
 
-        const createdChat = await Chat.create(chatData) ;
-        const FullChat = await Chat.findOne({_id : createdChat._id}).populate("users","-password") ;
-
-        res.status(200).send(FullChat) ;
+      res.status(200).send(FullChat);
     }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
+// fetch all chats
+export const fetchChat = async (req, res) => {
+  try {
+    // searching through all chats that the logged-in user is a part of
+    let Chats = await Chat.find({
+      users: { $elemMatch: { $eq: req.user._id } },
+    })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 });
+
+    Chats = await User.populate(Chats, {
+      path: "latestMessage.sender",
+      select: "name pic email",
+    });
+
+    res.status(200).send(Chats);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,12 +1,12 @@
 import express from "express" ;
 import { protect } from "../middleware/authMiddleware.js";
 
-import { accessChat } from "../controller/chatController.js";
+import { accessChat ,fetchChat } from "../controller/chatController.js";
 
 const chatRouter = express.Router() ;
 
 chatRouter.post("/",protect ,accessChat) ;
-// chatRouter.get("/all",protect ,fetchChat) ;
+chatRouter.get("/all",protect ,fetchChat) ;
 // chatRouter.post("/group/new",protect ,createGroupChat) ;
 // chatRouter.put("/group/rename",protect ,renameGroupChat) ;
 // chatRouter.put("/group/remove",protect ,removeFromGroup) ;
